8fb2ce76e59416db9c21f04bddc332a7cb24b64a
BojanStipic <bojanstipic@gmail.com>
2022-08-01 23:47:29 +0200

    Add license to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,33 @@
 import React, { FC } from "react";
-import { Flex } from "@chakra-ui/react";
+import { Text, Link } from "@chakra-ui/react";
 
 import { Card } from "./";
+import { useAccentColor } from "../hooks";
 
 export const Footer: FC = () => {
+  const linkColor = useAccentColor();
+
   return (
     <Card as="footer" variant="outline" p={4}>
-      <Flex fontSize="sm" fontWeight="light">
-        Copyright © 2022 Bojan Stipic
-      </Flex>
+      <Text fontSize="sm" fontWeight="light" textAlign="center">
+        Copyright © 2021-2022 Bojan Stipic | Website source code is available on{" "}
+        <Link
+          href="https://github.com/BojanStipic/website"
+          isExternal={true}
+          color={linkColor}
+        >
+          GitHub
+        </Link>
+        , licensed under{" "}
+        <Link
+          href="https://github.com/BojanStipic/website/blob/master/LICENSE"
+          isExternal={true}
+          color={linkColor}
+        >
+          AGPL-3.0-or-later
+        </Link>
+        .
+      </Text>
     </Card>
   );
 };
