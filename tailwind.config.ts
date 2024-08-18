65b2e80a5e8bf8895fbc6327c3dbf825d9319340
BojanStipic <bojanstipic@gmail.com>
2024-08-18 17:45:51 +0200

    Remove custom fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import catppuccinTheme from "@catppuccin/tailwindcss";
 import typography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
-import defaultTheme from "tailwindcss/defaultTheme";
 import type { PluginCreator } from "tailwindcss/types/config";
 
 // eslint-disable-next-line no-restricted-syntax
@@ -9,10 +8,6 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
-        mono: ["Iosevka", ...defaultTheme.fontFamily.mono],
-      },
       colors: {
         primary: "rgba(var(--ctp-mauve), <alpha-value>)",
         text: "rgba(var(--ctp-text), <alpha-value>)",
