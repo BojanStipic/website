331683ab142910d7c3f511e9424378e06c6ff3eb
BojanStipic <bojanstipic@gmail.com>
2024-10-28 13:19:42 +0100

    Use astro-icons

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,14 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 // eslint-disable-next-line no-restricted-syntax
 export default defineConfig({
   site: "https://bojanstipic.com",
-  integrations: [tailwind(), sitemap(), mdx()],
+  integrations: [tailwind(), sitemap(), mdx(), icon()],
   markdown: {
     shikiConfig: {
       themes: {
